b41d638c6b9f687cb0b427ca729471b1fe4880b2
parinder <prinderspacher@yahoo.de>
2025-05-15 12:45:49 +0000

    enshrouded
    -	corrected server not launching anymore after keen update.
    -	removed secrets handling and added a placeholder json config file.

--- config.sh
+++ config.sh
@@ -6,6 +6,13 @@ if [ -d "secrets" ]; then
   exit 0
 fi
 
+read -p "Enter the Country name : " SSL_C
+read -p "Enter the State name : " SSL_ST
+read -p "Enter the Locality name : " SSL_L
+read -p "Enter the Organization name : " SSL_O
+read -p "Enter the Organizational Unit name : " SSL_OU
+read -p "Enter the Common name : " SSL_CN
+
 echo "creating the .env file..."
 mkdir -p secrets
 cat <<EOF > .env
@@ -14,25 +21,17 @@ cat <<EOF > .env
 
 # PROJECT ZOMBOID
 PROJECT_ZOMBOID_ADMIN_PASS_FILE='/run/secrets/project_zomboid_admin_pass'
-
-# ENSHROUDED
-ENSHROUDED_ADMIN_PASS_FILE='/run/secrets/enshrouded_admin_pass'
-ENSHROUDED_FRIEND_PASS_FILE='/run/secrets/enshrouded_friend_pass'
-ENSHROUDED_DEFAULT_PASS_FILE='/run/secrets/enshrouded_default_pass'
 EOF
 
 echo "creating random secret passwords..."
 echo "$(openssl rand -base64 12)" > secrets/project_zomboid_admin_pass.txt
-echo "$(openssl rand -base64 12)" > secrets/enshrouded_admin_pass.txt
-echo "$(openssl rand -base64 12)" > secrets/enshrouded_friend_pass.txt
-echo "$(openssl rand -base64 12)" > secrets/enshrouded_default_pass.txt
 
-# echo "creating ssl key/crt..."
-# openssl req -x509 -newkey rsa:4096 \
-#     -keyout requirements/nginx/tools/ssl.key \
-#     -out requirements/nginx/tools/ssl.crt \
-#     -sha256 -days 3650 -nodes \
-#     -subj "/C=<?>/ST=<?>/L=<?>/O=<?>/OU=<?>/CN=<?>" \
-# 	> /dev/null 2>&1
+echo "creating ssl key/crt..."
+openssl req -x509 -newkey rsa:4096 \
+    -keyout requirements/nginx/tools/ssl.key \
+    -out requirements/nginx/tools/ssl.crt \
+    -sha256 -days 3650 -nodes \
+    -subj "/C=$SSL_C/ST=$SSL_ST/L=$SSL_L/O=$SSL_O/OU=$SSL_OU/CN=$SSL_CN" \
+	> /dev/null 2>&1
 
 echo "Done."
