cc69d430ea974b3ee83a532b788f847d1282e574
parinder <prinderspacher@yahoo.de>
2025-07-06 23:52:40 +0000

    satisfactory
    -	removed experimental beta version build.

--- requirements/satisfactory/tools/entrypoint.sh
+++ requirements/satisfactory/tools/entrypoint.sh
@@ -2,8 +2,9 @@
 
 # update server
 steamcmd \
+    +force_install_dir $HOME/SatisfactoryDedicatedServer \
 	+login anonymous \
-    +app_update 1690800 -beta experimental validate \
+    +app_update 1690800 validate \
     +quit
 
 # launch server
